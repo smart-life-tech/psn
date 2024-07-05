692051755c72dc7578985536873a0dd41157f480
smart-life-tech <christlightworld@gmail.com>
2024-07-05 14:38:09 +0100

    modified:   psn_to_sacn.py

--- psn_to_sacn.py
+++ psn_to_sacn.py
@@ -24,36 +24,14 @@ def start_psn():
 def fill_dmx(psn_data):
     if isinstance(psn_data, pypsn.psn_data_packet):
         position = psn_data.trackers[0].pos
-        speed=psn_data.trackers[0].speed
-        # status= psn_data.trackers[0].status
-        # timestamp= psn_data.trackers[0].timestamp
-        # trgpos=psn_data.trackers[0].info
         dmx_data = [0] * 512
         dmx_data[0] = int(abs(position.x))
         dmx_data[1] = int(abs(position.y))
         dmx_data[2] = int(abs(position.z))
-        print("postion : ",position)
-        print("speed :" ,speed)
-        # print("status :" ,status)
-        # print("timestamp :" ,timestamp)
-        # print("trgpos :" ,trgpos)
-        # print(dmx_data)
-        # if psn_data.trackers[0].vel.x > 0:
-        #     dmx_data[3] = int(abs(psn_data.trackers[0].vel.x))
-        # else:
-        #     dmx_data[3] = int(abs(psn_data.trackers[0].vel.x))
-        
-        #sender[1].dmx_data = dmx_data
 
-def fill_dmx(psn_data):
-    if isinstance(psn_data, pypsn.psn_data_packet):
-        position = psn_data.trackers[0].pos
-        speed=psn_data.trackers[0].speed
-        status= psn_data.trackers[0].status
-        timestamp= psn_data.trackers[0].timestamp
-        trgpos=psn_data.trackers[0].info
+        sender[1].dmx_data = dmx_data
+
 
 if __name__ == "__main__":
-    print("Starting DMX...")
     start_dmx()
     start_psn()
