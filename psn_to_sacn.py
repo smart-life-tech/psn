dc771b597b5f6847b389da5ad7775d77929c5d92
smart-life-tech <christlightworld@gmail.com>
2024-07-11 19:26:47 +0100

    modified:   psn_to_sacn.py

--- psn_to_sacn.py
+++ psn_to_sacn.py
@@ -27,7 +27,7 @@ def fill_dmx(psn_data):
     global prints
     if isinstance(psn_data, pypsn.psn_data_packet):
         if(prints):
-            print(psn_data.trackers)
+            print(psn_data.trackers[0])
             prints=False
             time.sleep(50)
         position = psn_data.trackers[0].pos
