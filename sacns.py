f960795faae930fec1c0cef17eddae9a549d82b4
smart-life-tech <christlightworld@gmail.com>
2024-07-13 14:35:16 +0100

    modified:   sacns.py

--- sacns.py
+++ sacns.py
@@ -1,6 +1,18 @@
 import sacn
 import time
-
+def scale_value( value, input_min, input_max, output_min, output_max):
+    input_range = input_max - input_min
+    output_range = output_max - output_min
+    scaled_value = (((value - input_min) * output_range) / input_range) + output_min
+    return scaled_value
+input1=100
+input2 =200
+outputmin=0
+outputmax=10
+for i in range (100,200):
+    
+    print("mapped : ",scale_value(i,input1,input2,outputmin,outputmax), "number :",i)
+    time.sleep(1)
 sender = sacn.sACNsender()  # provide an IP-Address to bind to if you want to send multicast packets from a specific interface
 sender.start()  # start the sending thread
 sender.activate_output(1)  # start sending out data in the 1st universe
