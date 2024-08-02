13755599fa86659566875ee4949824c453d0d9d6
smart-life-tech <christlightworld@gmail.com>
2024-08-02 19:56:44 +0100

    modified:   data_converter.py

--- data_converter.py
+++ data_converter.py
@@ -230,7 +230,7 @@ class DataConverter:
             
             coordinate_tuple = tuple(output_data.get(addr, 0) for addr in sorted(sacn_mappings.values()))
             print(f"Sending data  : {coordinate_tuple}")
-            sender[universe].dmx_data = coordinate_tuple
+            self.sender[universe].dmx_data = coordinate_tuple
             print(f"Sending data to sACN addresses: {output_data}")
             
             coordinate_tuple = (int(outputx), int(outputy), int(outputz))
